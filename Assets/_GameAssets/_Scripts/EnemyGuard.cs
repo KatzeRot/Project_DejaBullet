6624c1368bb961b0552a1b289a9ade7adb18ed68
KatzeRot <43536164+KatzeRot@users.noreply.github.com>
2018-12-11 01:28:31 +0100

    Initial commit

---
+++ Assets/_GameAssets/_Scripts/EnemyGuard.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
+public class EnemyGuard : MonoBehaviour {
+    private enum Status { Shooting, Idle, Waiting, Running }
+    Status status = Status.Idle;
+
+    private NavMeshAgent agent;
+    private Animator animatorEnemyGuard;
+    RaycastHit rch;
+
+    [Header("StatusCharacter&References")]
+    [SerializeField] bool state = true; //Check if is alive or death
+    [SerializeField] Transform positionCombat; //The position where he has to be when the PLAYER has been founded
+    [SerializeField] GameObject eyesWatcher;
+    [SerializeField] GameObject shootPoint;
+    [SerializeField] GameObject player;
+    private bool damaged = false;
+    [SerializeField] float health = 20;
+    [SerializeField] float distanceValue = 20.0f;
+    [SerializeField] float angleValue = 62f;
+    [SerializeField] int damage = 1;
+    [SerializeField] float cadence;
+    private float otherShoot = 0;
+
+
+    void Start() {
+        otherShoot = cadence;
+        agent = GetComponent<NavMeshAgent>();
+        animatorEnemyGuard = GetComponent<Animator>();
+    }
+    void Update() {
+        if (IsAlive()) {
+            otherShoot += 0.1f;
+            float distance = Vector3.Distance(transform.position, player.transform.position);
+            Vector3 direccion = Vector3.Normalize(player.transform.position - transform.position);
+            float angulo = Vector3.Angle(direccion, transform.forward);
+            Vector3 playerPosition = new Vector3(player.transform.position.x,
+                                            this.transform.position.y,
+                                            player.transform.position.z);
+            // if(damaged == true && status == Status.Idle) {
+            //     status = Status.Running;
+            // }else if(damaged == true && status == Status.Waiting){
+            //     status = Status.Shooting;
+            // }
+            switch (status) {
+                case Status.Idle: //When he doesn't see the PLAYER
+                    Ray ray = CreateRayCast(eyesWatcher.transform.position, eyesWatcher.transform.forward); ;
+                    if (distance < distanceValue && angulo < angleValue) {
+                        if (Physics.Raycast(ray, out rch, Mathf.Infinity) && rch.transform.gameObject.name == "HeroController") {
+                            print("Identificado: " + rch.collider.gameObject.name);
+                            status = Status.Running; //When the PLAYER has been detected, he pass to running status
+                        }
+                    }
+                    break;
+                case Status.Shooting: //When he has seen the PLAYER and has visual contact with him
+                    print("EMPEZANDO A DISPARAR");
+                    this.transform.LookAt(playerPosition);
+                    Ray rayShoot = CreateRayCast(shootPoint.transform.position, shootPoint.transform.forward);
+                    eyesWatcher.transform.LookAt(player.transform.position);
+                    shootPoint.transform.LookAt(player.transform.position);
+                    print("DEBERÍA DISPARAR Y ENCONTRAR AL PLAYER");
+                    if (Physics.Raycast(rayShoot, out rch, Mathf.Infinity) && rch.transform.gameObject.name != "HeroController") {
+                        print("Se ha perdido el CONTACTO con el PLAYER");
+                        //this.transform.LookAt(playerPosition);
+                        status = Status.Waiting; //When he arrived to his position, he pass to waiting status
+                    } else if (otherShoot >= cadence) {
+                        ShootToPlayer(rayShoot);
+                        animatorEnemyGuard.SetBool("Shooting", true);
+                    }
+                    break;
+                case Status.Waiting: //When he has seen the PLAYER but hasn't visual contact
+                    shootPoint.transform.rotation = new Quaternion(0f, 0f, 0f, 0f); //Default rotation of the generator bullet
+                    animatorEnemyGuard.SetBool("EnemyFounded", true);
+                    animatorEnemyGuard.SetBool("Shooting", false);
+                    ray = CreateRayCast(eyesWatcher.transform.position, eyesWatcher.transform.forward);
+                    if (Physics.Raycast(ray, out rch, Mathf.Infinity) && rch.transform.gameObject.name == "HeroController") {
+                        print("Identificado: " + rch.collider.gameObject.name);
+                        this.transform.LookAt(playerPosition);
+                        status = Status.Shooting; //When he arrived to his position, he pass to waiting status
+                    }
+                    break;
+                case Status.Running: //When he isn't in his position and is moving to there
+                    if(positionCombat != null) {
+                        animatorEnemyGuard.SetBool("Running", true);
+                        agent.destination = positionCombat.position;
+                    }else{
+                        status = Status.Waiting; //When he arrived to his position, he pass to waiting status
+                    }
+                    break;
+            }
+        }else{
+            DestroyImmediate(this.gameObject);
+        }
+
+    }
+    private void ShootToPlayer(Ray ray) {
+        //Ray rayShootEnemy = new Ray(eyesWatcher.transform.position, eyesWatcher.transform.forward);
+        float x = Random.Range(0, 5f);
+        float y = Random.Range(0, 5f);
+        shootPoint.transform.Rotate(x, y, 0);
+        animatorEnemyGuard.SetBool("Shooting", true);
+        otherShoot = 0;
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity)) {
+            if (hit.collider.gameObject.tag == "Player") {
+                print(hit.collider.gameObject.tag + " tocado");
+                hit.collider.gameObject.GetComponent<HeroPlayer>().TakeDamage(damage); //Realiza el daño al PLAYER
+                Debug.DrawLine(shootPoint.transform.position, shootPoint.transform.forward * Mathf.Infinity, Color.yellow, 1);
+            }
+            Debug.DrawLine(ray.origin, shootPoint.transform.forward * Mathf.Infinity, Color.yellow, 1);
+        }
+        Debug.DrawRay(shootPoint.transform.position, shootPoint.transform.forward * Mathf.Infinity, Color.magenta, 2);
+    }
+
+    private Ray CreateRayCast(Vector3 origin, Vector3 destination) {
+        eyesWatcher.transform.LookAt(player.transform.position);
+        Ray ray = new Ray(origin, destination);
+        return ray;
+    }
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "positionCombat") {
+            print("Tocado el TRIGGER");
+            animatorEnemyGuard.SetBool("Running", false);
+            agent.isStopped = true;
+            status = Status.Waiting; //When he arrived to his position, he pass to waiting status
+        }
+    }
+    private bool IsAlive() {
+        if(health <= 0){
+            state = false;
+        }
+        return state;
+    }
+    public void TakeDamage(float damage) {
+        //watcherAnimator.SetTrigger("Hit");
+        damaged = true;
+        health -= damage;
+    }
+}
---
+++ Assets/_GameAssets/_Scripts/HeroPlayer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeroPlayer : MonoBehaviour {
+    private enum StatusPlayer { Stop, Moving, Shooting, Jumping }
+    StatusPlayer status = StatusPlayer.Stop;
+
+    [SerializeField] const int TOTAL_HEALTH = 100;
+    [SerializeField] int health = TOTAL_HEALTH;
+    [SerializeField] Text healthText;
+    private const int TOTAL_WEAPONS = 2;
+    [SerializeField] Weapon[] weapons = new Weapon[TOTAL_WEAPONS];
+    private int equipedWeapon = 0;
+
+    private bool crounched = true;
+    private bool state;
+
+    void Start() {
+
+    }
+    void Update() {
+        healthText.text = health + " / " + TOTAL_HEALTH;
+        ShootAction();
+        CrouchAction();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+
+        switch (status) {
+            case StatusPlayer.Stop:
+                break;
+            case StatusPlayer.Shooting:
+                ShootAction();
+                break;
+                //case StatusPlayer.Jumping:
+                //    JumpAction();
+                //    break;
+        }
+    }
+    public void ShootAction() {
+        if (Input.GetMouseButton(0)) {
+            //weapons[equipedWeapon].GetComponent<Animator>().SetBool("Shooting", true);
+            weapons[equipedWeapon].PullTrigger();
+        }else{
+            weapons[equipedWeapon].GetComponent<Animator>().SetBool("Shooting", false);
+        }
+    }
+    private void JumpAction() {
+        //For now, doesn't has any use
+    }
+    private void CrouchAction() {
+        if (Input.GetKeyDown(KeyCode.C)) {
+            if (crounched) {
+                GetComponent<CharacterController>().height = 2f;
+                crounched = false;
+            } else {
+                GetComponent<CharacterController>().height = 4.6f;
+                crounched = true;
+            }
+        }
+    }
+    private bool IsAlive() {
+        return state;
+    }
+    public void TakeDamage(int damage) {
+        print("PLAYER recibe DAMAGES");
+        health -= damage;
+    }
+}
